72b9a65ee9fe5208c3f2ff110d1aa17b6b61b731
PraptiH <praptiantara1892@gmail.com>
2025-05-02 10:57:21 +0600

    fix the button issue

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn5 = document.getElementById('btn-5');
     const btn6 = document.getElementById('btn-6');
     const btn7 = document.getElementById('btn-7');
-    const btn8 = document.getElementById('btn-11');
     const activityLog = document.querySelectorAll('.activityLog');
 
     const calendar = new Date();
@@ -31,38 +30,48 @@ document.addEventListener('DOMContentLoaded', () => {
                 const activity1 = document.getElementById('activity1');
                 activity1.innerText = `You have completed the task Fix Mobile Button Issue at ${time}`;
                 activity1.classList.remove('hidden');
-                btn1.classList.add('hidden');
-                btn8.classList.remove('hidden');
+                btn1.style.backgroundColor = "#a0aec0"
+                btn1.disabled =true;
             }
             if (btn == btn2) {
                 alert("Board Updated Succesfully");
                 const activity2 = document.getElementById('activity2');
                 activity2.innerText = `You have completed the task Add Dark Mode at ${time}`;
                 activity2.classList.remove('hidden');
+                btn2.style.backgroundColor = "#a0aec0"
+                btn2.disabled =true;
             }
             if (btn == btn3) {
                 alert("Board Updated Succesfully");
                 const activity3 = document.getElementById('activity3');
                 activity3.innerText = `You have completed the task Optimize Home Page at ${time}`;
                 activity3.classList.remove('hidden');
+                btn3.style.backgroundColor = "#a0aec0"
+                btn3.disabled =true;
             }
             if (btn == btn4) {
                 alert("Board Updated Succesfully");
                 const activity4 = document.getElementById('activity4');
                 activity4.innerText = `You have completed the task Add New Emoji at ${time}`;
                 activity4.classList.remove('hidden');
+                btn4.style.backgroundColor = "#a0aec0"
+                btn4.disabled =true;
             }
             if (btn == btn5) {
                 alert("Board Updated Succesfully");
                 const activity5 = document.getElementById('activity5');
                 activity5.innerText = `You have completed the task Integrate OpenAI API at ${time}`;
                 activity5.classList.remove('hidden');
+                btn5.style.backgroundColor = "#a0aec0"
+                btn5.disabled =true;
             }
             if (btn == btn6) {
                 alert("Board Updated Succesfully");
                 const activity6 = document.getElementById('activity6');
                 activity6.innerText = `You have completed the task Improve Job Searching at ${time}`;
                 activity6.classList.remove('hidden');
+                btn6.style.backgroundColor = "#a0aec0"
+                btn6.disabled =true;
             }
 
             if (convertedTextAssigned == 1) {
